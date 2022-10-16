54a61bc0f419a23998a848f122376a353e5dabcc
Riszterer Istvan <gwlxts@gmail.com>
2022-10-16 19:42:32 +0200

    tesztelés

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Chess");
+        
+        //tesztelések 
+        RisztererIstvan.run();
     }
 }
