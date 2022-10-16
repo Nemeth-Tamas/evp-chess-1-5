0a120a0f10d39a8f3fa9c3329b11215eb6a55478
Riszterer Istvan <gwlxts@gmail.com>
2022-10-16 19:32:35 +0200

    A  moveset possiblemoves() function-je implementálja a bábuk (kiráyon és a paraszton kivül) legtágabban értelmezett lépési lehetőséginek kiszámítását (, az az csak a tábla 8x8-as jellege és a bábu lépési típusa jelenik meg szürő tényezőként)...

--- src/main/java/MoveSet.java
+++ src/main/java/MoveSet.java
@@ -1,6 +1,52 @@
-import java.util.ArrayList;
 
-public class MoveSet{
+import java.util.ArrayList; 
+
+public abstract class MoveSet{
     public static ArrayList<Coords> moves;
     public static boolean repeating;
+    public abstract MoveSet getSingleton();
+    public ArrayList<Coords> possibleMoves(final Coords positionOfPiece)
+    {
+        ArrayList<Coords> result=new ArrayList<Coords>();
+        if(repeating){
+            for(int i=0; i<getSingleton().moves.size();i++)
+            {
+                final Coords currentMove = getSingleton().moves.get(i);
+                //hozzáadja az adott lépés típus kordinátáit
+                for(Coords currentTile = new Coords(0,0).add(positionOfPiece).add(currentMove); 
+                    (currentTile.getX()<=7 && currentTile.getX()>=0 && currentTile.getY()<=7 && currentTile.getY()>=0);
+                    currentTile.add(currentMove))
+                {
+                    result.add(new Coords(currentTile));           
+                }
+                //kivonja az adott lépés típus kordinátáit
+                for(Coords currentTile = new Coords(0,0).add(positionOfPiece).subtract(currentMove); 
+                    (currentTile.getX()<=7 && currentTile.getX()>=0 && currentTile.getY()<=7 && currentTile.getY()>=0);
+                    currentTile.subtract(currentMove))
+                {
+                    result.add(new Coords(currentTile));           
+                } 
+            }
+        }else{
+            Coords currentTile;
+            for(int i=0; i<getSingleton().moves.size();i++)
+            {
+                final Coords currentMove = getSingleton().moves.get(i);
+                                
+                //hozzáadja az adott lépés típus kordinátáit
+                currentTile = Coords.add(positionOfPiece,getSingleton().moves.get(i));
+                if(currentTile.getX()<=7 && currentTile.getX()>=0 && currentTile.getY()<=7 && currentTile.getY()>=0)
+                {
+                    result.add(new Coords(currentTile));           
+                }
+                //kivonja az adott lépés típus kordinátáit
+                currentTile = Coords.subtract(positionOfPiece,getSingleton().moves.get(i));
+                if(currentTile.getX()<=7 && currentTile.getX()>=0 && currentTile.getY()<=7 && currentTile.getY()>=0)
+                {
+                    result.add(new Coords(currentTile));           
+                }
+            }
+        }
+        return result;
+    }
 }
