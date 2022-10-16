b594676088de126ad943372855e18cec2270d4d5
Riszterer Istvan <gwlxts@gmail.com>
2022-10-16 19:40:54 +0200

    Kordináta class-t egy kicsit kibővítettem. ADD METODUS - osztály szinten és objektumra is implementálva , GETTERS, SETTERS , TOSTRING

--- src/main/java/Coords.java
+++ src/main/java/Coords.java
@@ -1,4 +1,40 @@
+
 public class Coords {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
+    
+    public Coords(int x,int  y){this.x = x; this.y = y;}
+    public Coords(Coords a){this.x = a.getX(); this.y = a.getY();}
+
+    public int getY(){return this.y;}
+    public int getX(){return this.x;}  
+    public void setX(int a){this.x = a;}
+    public void setY(int a){this.y = a;}
+    
+    public Coords add(Coords a)
+    {
+        this.x += a.getX();
+        this.y += a.getY(); 
+        return this;
+    }
+    public Coords subtract(Coords a)
+    {
+        this.x -= a.getX();
+        this.y -= a.getY();
+        return this;
+    }
+    public static  Coords add(Coords a, Coords b)
+    { 
+        return new Coords(a.getY()+b.getY(), a.getX() + b.getX()); 
+    }
+    public static Coords subtract(Coords a, Coords b)
+    { 
+        return new Coords(a.getY()-b.getY(), a.getX() - b.getX()); 
+    }
+
+    @Override
+    public String toString() {
+        return ("(x: "+this.x+", y: "+this.y+")");
+    }
+
 }
